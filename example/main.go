d051d032801e8bce25a38c8e87f7db815f75e077
Gerardo Reyes <grevych@gmail.com>
2018-05-23 12:19:38 -0500

    Update examples to match the new way to load cfg

--- example/main.go
+++ example/main.go
@@ -9,21 +9,17 @@ import (
 
 var (
 	configPath string
-	configName string
 )
 
 func init() {
-	flag.StringVar(&configPath, "configPath", "./config", "Path to the configuration directory.")
-	flag.StringVar(&configName, "configName", "local", "Running configuration name.")
+	flag.StringVar(&configPath, "configPath", "", "Path to the configuration directory.")
+	cfg.Load(&cfg.Cfg{
+		SvcName: "main",
+		Path:    configPath,
+	})
 }
 
 func main() {
-	flag.Parse()
-	config, err := cfg.LoadConfig(configPath, configName)
-	if err != nil {
-		fmt.Printf("Error loading config: %+v", err)
-		return
-	}
-
-	fmt.Printf("%+v", config)
+	svcCfg := cfg.Get("services.main")
+	fmt.Printf("%+v", svcCfg)
 }
