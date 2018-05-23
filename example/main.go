e298f89df95f9c1b1dfb8b5494ce3c26fd7488f2
mmiranda96 <mikemp96@gmail.com>
2018-05-23 14:49:27 -0500

    Modify example for new changes.

--- example/main.go
+++ example/main.go
@@ -1,25 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
 
 	"github.com/crabi/cfg"
 )
 
-var (
-	configPath string
-)
-
-func init() {
-	flag.StringVar(&configPath, "configPath", "", "Path to the configuration directory.")
-	cfg.Load(&cfg.Cfg{
-		SvcName: "main",
-		Path:    configPath,
-	})
-}
-
 func main() {
-	svcCfg := cfg.Get("services.main")
-	fmt.Printf("%+v", svcCfg)
+	// Retrieving the required values (config path and config file type) is up to the user
+	var configPath string
+	flag.StringVar(&configPath, "configPath", "", "Path to the configuration directory.")
+	flag.Parse()
+
+	// Config should be loaded before any other code functionality
+	config, err := cfg.Load(&cfg.Params{Path: configPath})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Nested configurations can be retrieved using dot notation
+	mainServiceConfig := config.Get("services.main")
+
+	// Only to pretty print
+	mainServiceConfigPretty, err := json.MarshalIndent(mainServiceConfig, "", "  ")
+	fmt.Printf("%s", string(mainServiceConfigPretty))
 }
