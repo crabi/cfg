30083e1d90c1fb641220fed2e1810a1d2849b56d
mmiranda96 <mikemp96@gmail.com>
2018-05-23 14:49:07 -0500

    Fix refactor and tests.

--- cfg.go
+++ cfg.go
@@ -1,88 +1,93 @@
 package cfg
 
 import (
-	"log"
+	"errors"
+	"fmt"
 	"os"
-	"strings"
 
 	"github.com/spf13/viper"
 )
 
 const (
-	envModeVar        = "GOENV"
-	defaultFileType   = "yaml"
-	defaultConfigEnv  = "default"
+	envNameKey = "GOENV"
+
 	defaultConfigPath = "./config"
-	localConfigEnv    = "local"
+	defaultConfigType = "viper"
+	defaultFileType   = "yaml"
+
+	defaultConfigEnv = "default"
+	localConfigEnv   = "local"
 )
 
-type Cfg struct {
-	SvcName  string
+// Params is used to load a new environment
+type Params struct {
 	Path     string
 	FileType string
 }
 
-type cfg interface {
-	getEnv() string
-	loadEnv(string) error
-	mergeEnv(string) error
-	setCoreUp()
-	setDefaults()
-}
-
-// Load loads a config directory, the default config file and overrides with a given configuration.
-// It returns a generic configuration, which ideally will be parsed into a struct.
-func Load(c cfg) {
-	c.setDefaults()
-	c.setCoreUp()
-
-	if err := c.loadEnv(defaultConfigEnv); err != nil {
-		log.Fatalln("error reading default configuration file:", err)
-	}
-
-	env := c.getEnv()
-	if err := c.mergeEnv(env); err != nil {
-		log.Fatalln("error reading %s configuration file: %s", env, err)
-	}
-}
-
-func (c *Cfg) loadEnv(env string) error {
-	viper.SetConfigName(env)
-	viper.AddConfigPath(c.Path)
-	return viper.ReadInConfig()
-}
-
-func (c *Cfg) mergeEnv(env string) error {
-	viper.SetConfigName(env)
-	viper.AddConfigPath(c.Path)
-	return viper.MergeInConfig()
-}
-
-func (c *Cfg) setDefaults() {
+// SetDefaults sets the default values of a Config fields
+func (c *Params) SetDefaults() {
 	if c.Path == "" {
 		c.Path = defaultConfigPath
 	}
-
 	if c.FileType == "" {
 		c.FileType = defaultFileType
 	}
 }
 
-func (c *Cfg) setCoreUp() {
-	viper.SetConfigType(c.FileType)
-	viper.SetEnvPrefix(c.SvcName)
-	viper.AutomaticEnv()
-	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+type configWrapper struct {
+	v      *viper.Viper
+	params *Params
 }
 
-func (c *Cfg) getEnv() string {
-	env, ok := os.LookupEnv(envModeVar)
-	if !ok || env == "" {
-		env = localConfigEnv
+func (c *configWrapper) getEnv() string {
+	envName := os.Getenv(envNameKey)
+	if envName == "" {
+		envName = localConfigEnv
 	}
-	return env
+	return envName
 }
 
-func Get(key string) map[string]interface{} {
-	return viper.GetStringMap(key)
+func (c *configWrapper) loadEnv(envName string) error {
+	c.v.SetConfigType(c.params.FileType)
+	c.v.SetConfigName(envName)
+	c.v.AddConfigPath(c.params.Path)
+	return c.v.ReadInConfig()
+}
+
+func (c *configWrapper) mergeEnv(envName string) error {
+	c.v.SetConfigType(c.params.FileType)
+	c.v.SetConfigName(envName)
+	c.v.AddConfigPath(c.params.Path)
+	return c.v.MergeInConfig()
+}
+
+// Get returns a map[string]interface{} of a given key
+func (c *configWrapper) Get(key string) map[string]interface{} {
+	return c.v.GetStringMap(key)
+}
+
+// Load loads a config directory, the default config file and overrides with a given configuration.
+// It returns a generic configuration, which ideally will be parsed into a struct.
+func Load(params *Params) (*configWrapper, error) {
+	if params == nil {
+		return nil, errors.New("nil params")
+	}
+
+	c := &configWrapper{
+		v:      viper.New(),
+		params: &(*params),
+	}
+	c.params.SetDefaults()
+
+	if err := c.loadEnv(defaultConfigEnv); err != nil {
+		return nil, fmt.Errorf("error reading \"default\" configuration file")
+	}
+
+	envName := c.getEnv()
+	if err := c.mergeEnv(envName); err != nil {
+		return nil, fmt.Errorf("error merging \"%s\" configuration file", envName)
+	}
+
+	return c, nil
 }
